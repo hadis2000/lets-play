cfe77cc318b01caeba6adbaddbe654bcb579fd10
hadis2000 <hadisehoseini6301@gmail.com>
2025-06-02 11:17:10 +0330

    refactor: comment out unused routing code in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/tic-tac-toe-online/home";
-import Game from "./pages/tic-tac-toe-online/game";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./pages/tic-tac-toe-online/home";
+// import Game from "./pages/tic-tac-toe-online/game";
+
+import TicTacToe from "./pages/tic-tac-toe";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/game" element={<Game />} />
-      </Routes>
-    </Router>
+    <TicTacToe />
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/game" element={<Game />} />
+    //   </Routes>
+    // </Router>
   );
 }
 
